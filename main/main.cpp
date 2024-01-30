4917c44a81427af7ecc85614d2137d293ccc5fc0
Huzaifa639 <mhuzaifaiiui@gmail.com>
2024-01-30 11:08:07 +0500

    updated_mpu6050_esp32

--- main/main.cpp
+++ main/main.cpp
@@ -16,14 +16,6 @@
 #include <math.h>
 #include "driver/uart.h"
 #include "esp_log.h"
-#include "esp_bt.h"
-#include "esp_bt_main.h"
-#include "esp_gap_bt_api.h"
-#include "esp_bt_device.h"
-#include "esp_spp_api.h"
-#include "time.h"
-#include "sys/time.h"
-
 int i2c_read(int addr, int start, uint8_t *buffer, int size);
 int i2c_write_reg(int addr, int reg, uint8_t data);
 void calibrate(void);
